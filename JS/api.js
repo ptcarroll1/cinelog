69903275f4b8ccddb2a2927142ebe33696741c7e
Patrick T. Carroll <carrolpt@dukes.jmu.edu>
2026-04-14 15:33:55 -0400

    made function for fetching movie data

---
+++ JS/api.js
@@ -0,0 +1,26 @@
+const API_KEY = "b008ed6c";
+const API_URL = "https://www.omdbapi.com/";
+
+
+async function fetchMovieData(title) {
+    const url = `${API_URL}?apikey=${API_KEY}&s=${encodeURIComponent(title)}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    if (data.Response === "False") {
+        throw new Error(`API error! message: ${data.Error}`);
+    }
+    return data.Search.map(movie => ({
+        imdbID: movie.imdbID,
+        title: movie.Title,
+        year: movie.Year,
+        poster: movie.Poster,
+    }));
+
+    
+}
+
